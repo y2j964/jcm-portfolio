71d6b38f9c948f4d89f7e844cdd45bf7a83132d2
Justin Mooney <y2j964@aol.com>
2020-10-28 21:21:27 -0400

    Move all files with associated files to own dir
    If a component has its own associated styles file or anything else
    assoiated with it (e.g. types), it is cleaner to create a dir for that
    component.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Navbar from "./Navbar/Navbar"
+import Footer from "./Footer/Footer"
 
 const Layout = ({ location, title, children }) => {
   return (
