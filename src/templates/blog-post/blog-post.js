1b643ef14a1b328ab4b1e29bdb058518b28a781c
Justin Mooney <y2j964@aol.com>
2020-12-18 20:19:35 -0500

    Move Bio into its own dir
    We will take this pattern everytime a component has associated styles.

--- src/templates/blog-post/blog-post.js
+++ src/templates/blog-post/blog-post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 
-import Bio from '../../components/bio';
+import Bio from '../../components/Bio/bio';
 import Layout from '../../components/layout';
 import SEO from '../../components/seo';
 
